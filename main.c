3f809a37090f16f72711a42b81d1c79b467a8b63
kryshac <vladislav.tornea@gmail.com>
2017-12-10 00:17:44 +0200

    update

--- main.c
+++ main.c
@@ -6,15 +6,23 @@
 /*   By: ccristia <ccristia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 15:40:14 by ccristia          #+#    #+#             */
-/*   Updated: 2017/12/09 17:08:00 by ccristia         ###   ########.fr       */
+/*   Updated: 2017/12/10 00:16:20 by ccristia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int **ft_movetop(int **elm)
+typedef struct	list
+{
+	int 		map[4][2];
+	struct list	*next;
+	struct list *back;
+}				t_nod;
+
+int		ft_movetop(int elm[4][2], t_nod *list)
 {
 	int	i;
 	int	minx;
@@ -34,14 +42,15 @@ int **ft_movetop(int **elm)
 	i = 0;
 	while (i < 4)
 	{
-		elm[i][0] -= minx;
-		elm[i][1] -= miny;
+		list->map[i][0] = elm[i][0] - minx;
+		list->map[i][1] = elm[i][1] - miny;
 		i++;
 	}
-	return (elm);
+	list->next = NULL;
+	return (1);
 }
 
-int	ft_elemcheck(char *s, int i)
+int		ft_elemcheck(char *s, int i)
 {
 	int v;
 
@@ -57,45 +66,65 @@ int	ft_elemcheck(char *s, int i)
 	return (v);
 }
 
-int	**ft_checkelm(char *s)
+int		ft_checkelm(char *s, t_nod *list)
 {
 	int 	i;
 	int 	v;
 	int 	pix;
-	int				elm[4][2];
+	int		elm[4][2];
+	int		nr;
 
-	i = 0;
+	i = -1;
 	v = 0;
 	pix = 4;
-	while (s[i])
+	nr = 0;
+	while (s[++i])
 	{
 		if (s[i] == '#')
 		{
-			elm[v][0] = i / 5;
-			elm[v][1] = i - i / 5 * 5;
+			elm[nr][0] = i / 5;
+			elm[nr++][1] = i - i / 5 * 5;
 			v += ft_elemcheck(s, i);
 			pix--;
 		}
-		else if ((i % 5 == 4 && s[i] != '\n') || (i != 20 && i % 5 != 4 &&
-			s[i] != '.') || (i == 20 && s[i] != '\n'))
-			return (NULL);
-		i++;
+		else if (((i % 5 == 4 && s[i] != '\n') || (i != 20 && i % 5 != 4 &&
+			s[i] != '.') || (i == 20 && s[i] != '\n')) || (i == 20 &&
+				(pix < 0 || !(v == 6 || v == 8))))
+			return (0);
 	}
-	if (pix < 0 || !(v == 6 || v == 8))
-		return (NULL);
-	return (ft_movetop(elm));
+	return (ft_movetop(elm, list));
 }
 
 int	ft_read(int mapfile)
 {
+	t_nod	*list;
+	t_nod	*tmp;
 	char	buff[22];
+	int		i;
 
+	list = (t_nod *)malloc(sizeof(t_nod));
+	if (list == NULL)
+		return (0);
+	list->back = NULL;
 	while (read(mapfile, &buff, 21))
 	{
 		buff[21] = '\0';
-		printf("\n\n");
-		if (ft_checkelm(buff) == 0)
+		if (ft_checkelm(buff, list) == 0)
 			return (0);
+		list->next = (t_nod *)malloc(sizeof(t_nod));
+		tmp = list;
+		list = list->next;
+		list->back = tmp;
+	}
+	while (tmp != NULL)
+	{
+		i = 0;
+		while (i < 4)
+		{
+			printf("x - |%d|, y - |%d|\n", tmp->map[i][0], tmp->map[i][1]);
+			i++;
+		}
+		tmp = tmp->back;
 	}
 	return (1);
 }
