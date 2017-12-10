25bee3b2ccdbf10cfe544539047f4ca6333f2ec2
kryshac <vladislav.tornea@gmail.com>
2017-12-10 18:51:09 +0200

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ccristia <ccristia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 15:40:14 by ccristia          #+#    #+#             */
-/*   Updated: 2017/12/10 00:16:20 by ccristia         ###   ########.fr       */
+/*   Updated: 2017/12/10 18:50:46 by ccristia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,39 @@ typedef struct	list
 	struct list *back;
 }				t_nod;
 
-int		ft_movetop(int elm[4][2], t_nod *list)
+t_nod		*ft_movetop(int elm[4][2], t_nod *list)
 {
-	int	i;
-	int	minx;
-	int	miny;
+	t_nod	*nod;
+	int		i;
+	int		minx;
+	int		miny;
 
-	i = 0;
+	i = -1;
 	minx = 4;
 	miny = 4;
-	while (i < 4)
+	while (++i < 4)
 	{
 		if (elm[i][0] < minx)
 			minx = elm[i][0];
 		if (elm[i][1] < miny)
 			miny = elm[i][1];
-		i++;
 	}
-	i = 0;
-	while (i < 4)
+	nod = (t_nod *)malloc(sizeof(t_nod));
+	if (nod == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < 4)
 	{
-		list->map[i][0] = elm[i][0] - minx;
-		list->map[i][1] = elm[i][1] - miny;
-		i++;
+		nod->map[i][0] = elm[i][0] - minx;
+		nod->map[i][1] = elm[i][1] - miny;
 	}
-	list->next = NULL;
-	return (1);
+	nod->next = NULL;
+	if (list != NULL)
+	{
+		list->next = nod;
+		nod->back = list;
+	}
+	return (nod);
 }
 
 int		ft_elemcheck(char *s, int i)
@@ -66,7 +73,7 @@ int		ft_elemcheck(char *s, int i)
 	return (v);
 }
 
-int		ft_checkelm(char *s, t_nod *list)
+t_nod		*ft_checkelm(char *s, t_nod *list)
 {
 	int 	i;
 	int 	v;
@@ -90,7 +97,7 @@ int		ft_checkelm(char *s, t_nod *list)
 		else if (((i % 5 == 4 && s[i] != '\n') || (i != 20 && i % 5 != 4 &&
 			s[i] != '.') || (i == 20 && s[i] != '\n')) || (i == 20 &&
 				(pix < 0 || !(v == 6 || v == 8))))
-			return (0);
+			return (NULL);
 	}
 	return (ft_movetop(elm, list));
 }
@@ -98,33 +105,27 @@ int		ft_checkelm(char *s, t_nod *list)
 int	ft_read(int mapfile)
 {
 	t_nod	*list;
-	t_nod	*tmp;
 	char	buff[22];
 	int		i;
 
-	list = (t_nod *)malloc(sizeof(t_nod));
-	if (list == NULL)
-		return (0);
-	list->back = NULL;
+	list = NULL;
 	while (read(mapfile, &buff, 21))
 	{
 		buff[21] = '\0';
-		if (ft_checkelm(buff, list) == 0)
+		if ((list = ft_checkelm(buff, list)) == NULL)
 			return (0);
-		list->next = (t_nod *)malloc(sizeof(t_nod));
-		tmp = list;
-		list = list->next;
-		list->back = tmp;
 	}
-	while (tmp != NULL)
+	while (list->back != NULL)
+		list = list->back;
+	while (list != NULL)
 	{
 		i = 0;
 		while (i < 4)
 		{
-			printf("x - |%d|, y - |%d|\n", tmp->map[i][0], tmp->map[i][1]);
+			printf("x - |%d|, y - |%d|\n", list->map[i][0], list->map[i][1]);
 			i++;
 		}
-		tmp = tmp->back;
+		list = list->next;
 	}
 	return (1);
 }
@@ -132,6 +133,10 @@ int	ft_read(int mapfile)
 int	main(int argc, char **argv)
 {
 	int	mapfile;
+	int a = 9;
+	int b = 5;
+	int c = 4;
+	int d = 3;
 
 	if (argc == 2)
 	{
@@ -146,5 +151,8 @@ int	main(int argc, char **argv)
 				printf("mapa defecta\n");
 		}
 	}
+	mapfile = ((a > b) ? b : a) > ((c > d) ? d : c) ?
+		((c > d) ? d : c) : ((a > b) ? b : a);
+	printf("numar - |%d|\n", mapfile);
 	return (0);
 }
